31b557e824fd607c00cafe6da5f71afc4c147331
hossain <guljer.net@gmail.com>
2023-06-22 14:45:25 +0600

    add alert

--- src/Api/order.js
+++ src/Api/order.js
@@ -1,13 +1,18 @@
-export const OrderPlace = data =>{
-  fetch(`https://car-doctor-server-guljer77.vercel.app/bookings`,{
+import Swal from "sweetalert2";
+
+export const OrderPlace = data => {
+  fetch(`https://car-doctor-server-guljer77.vercel.app/bookings`, {
     method: "POST",
     headers: {
-      "content-type":"application/json"
+      "content-type": "application/json",
     },
-    body: JSON.stringify(data)
+    body: JSON.stringify(data),
   })
-  .then(res=>res.json())
-  .then(data=>{
-    console.log(data);
-  })
-}
+    .then(res => res.json())
+    .then(data => {
+      console.log(data);
+      if (data.insertedId) {
+        Swal.fire("Order Confirm");
+      }
+    });
+};
